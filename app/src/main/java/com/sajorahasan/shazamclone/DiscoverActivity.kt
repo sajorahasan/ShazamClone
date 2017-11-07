e564d0dadca861ed9937ff029fbb5f111f5d42b3
abhishek <abhishekjirawla>
2017-11-07 15:03:02 +0530

    Initial commit

---
+++ app/src/main/java/com/sajorahasan/shazamclone/DiscoverActivity.kt
@@ -0,0 +1,12 @@
+package com.sajorahasan.shazamclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscoverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discover)
+    }
+}
